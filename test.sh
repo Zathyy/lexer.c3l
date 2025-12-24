fbab079630323e9655ec105e6e55a8609e06b1f5
Koni Marti <koni.marti@gmail.com>
2025-12-25 00:45:08 +0100

    ci: update test script

--- test.sh
+++ test.sh
@@ -9,10 +9,14 @@ c3c build lexv
 stdlib=$(c3c --build-env build lexv | awk '/^Stdlib/{print $3}')
 echo "Using the C3 standard libary in: ${stdlib}"
 
+# Locate unit tests
+unittests="$(dirname $stdlib)/test/unit"
+echo "Using the unit tests in: ${unittests}"
+
 # Collect all files from the C3 standard library and unit tests.
 files=()
 files+=( $(find ${stdlib} -name "*.c3") )
-files+=( $(find ${stdlib}/../test/unit -name "*.c3") )
+files+=( $(find ${unittests} -name "*.c3") )
 
 # Run the validation against the `c3c` compiler lexical output.
 c3c -E compile-only ${files[@]} | build/lexv
